aaa5de6167dd6272a4f197835e977ec5b8b34cb4
Prosenjit Manna <prosenjit@itobuz.com>
2023-10-02 12:59:12 +0530

    lint added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express!');
 });
 
 app.listen(port, () => {
