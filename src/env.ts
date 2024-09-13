8c8588c4692bc651aa0829e57b95a103aff8804a
Soumabha Roy <soumabha@itobuz.com>
2024-09-13 15:12:03 +0530

    fix: database connection

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,6 @@ import { cleanEnv, num, str } from 'envalid';
 export const appEnv = cleanEnv(process.env, {
   APPLICATION_PORT: num({ default: 4000 }),
   APPLICATION_URL: str({ default: 'localhost' }),
-  DATABASE_URL: str({ desc: 'DB URL' }),
+  DATABASE_URL: str({ desc: 'mongodb+srv://soumabha:1234@cluster0.dz4j9.mongodb.net/' }),
   NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'], default: 'development' }),
 });
--- src/seed.ts
+++ src/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, UserType } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
@@ -6,12 +6,8 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.user.create({
     data: {
+      name: faker.person.fullName(),
       email: faker.internet.email(),
-      password: faker.internet.password(),
-      phoneNo: faker.phone.number(),
-      verificationToken: faker.string.uuid(),
-      userType: UserType.CUSTOMER,
-      roleId: faker.string.uuid(),
     },
   });
   await prisma.$disconnect();
